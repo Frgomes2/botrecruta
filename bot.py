2d3e43af22fa498baa22aa59f77b8a46b84d3132
Frgomes <flavio.raphael@msn.com>
2025-06-23 19:28:40 -0300

    config bd

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-# Importações
 import os
-import mysql.connector
+import psycopg2
 from twitchio.ext import commands
 from dotenv import load_dotenv
 from comandos.presentes_comando import PresentesComando
@@ -13,10 +12,8 @@ from comandos.personagem_comando import PersonagemComando
 from comandos.poke_comando import PokemonComando
 from comandos.roubar_comando import RoubarComando
 
-# Carregar as variáveis de ambiente do arquivo .env
 load_dotenv()
 
-# Obter as variáveis de ambiente
 token = os.getenv("TWITCH_TOKEN")
 client_id = os.getenv("TWITCH_CLIENT_ID")
 bot_nick = os.getenv("TWITCH_BOT_NICK")
@@ -25,12 +22,11 @@ db_host = os.getenv("DB_HOST")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 db_name = os.getenv("DB_NAME")
+db_port = os.getenv("DB_PORT")
 
-# Verificar se as variáveis estão configuradas
 if not token or not client_id or not bot_nick or not channel:
     raise ValueError("Faltando configurações no arquivo .env")
 
-# Classe do bot
 class MyBot(commands.Bot):
 
     def __init__(self):
@@ -41,26 +37,24 @@ class MyBot(commands.Bot):
             prefix="!fr",
             initial_channels=[channel]
         )
-        # Conectar ao banco de dados
         self.db_connection = self.connect_to_db()
 
     def connect_to_db(self):
-        """Conectar ao banco de dados MySQL."""
         try:
-            connection = mysql.connector.connect(
-                host=db_host,
+            conn = psycopg2.connect(
+                dbname=db_name,
                 user=db_user,
                 password=db_password,
-                database=db_name
+                host=db_host,
+                port=db_port
             )
             print("Conexão com o banco de dados estabelecida com sucesso.")
-            return connection
-        except mysql.connector.Error as err:
+            return conn
+        except psycopg2.Error as err:
             print(f"Erro ao conectar ao banco de dados: {err}")
             return None
 
     def close_db_connection(self):
-        """Fecha a conexão com o banco de dados."""
         if self.db_connection:
             self.db_connection.close()
             print("Conexão com o banco de dados fechada.")
@@ -69,21 +63,17 @@ class MyBot(commands.Bot):
         print(f'Bot conectado como {self.nick}')
 
     async def event_message(self, message):
-        # Adiciona o processamento dos comandos recebidos
         await self.handle_commands(message)
 
     def run(self):
-        """Inicia o bot e configura o fechamento da conexão ao final."""
         try:
             super().run()
         finally:
             self.close_db_connection()
 
 
-# Criação da instância do bot
 bot = MyBot()
 
-# Registra os Cogs (comandos) no bot, passando a conexão como argumento
 bot.add_cog(PresentesComando(bot))
 bot.add_cog(VotacaoComando(bot))
 bot.add_cog(TinderComando(bot))
@@ -94,7 +84,5 @@ bot.add_cog(PersonagemComando(bot))
 bot.add_cog(PokemonComando(bot))
 bot.add_cog(RoubarComando(bot))
 
-
-# Rodar o bot
 if __name__ == "__main__":
     bot.run()
